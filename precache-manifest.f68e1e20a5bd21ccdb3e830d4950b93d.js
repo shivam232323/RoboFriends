725b12f26692c370b089759497965fc6ad020d7e
shivam232323 <66739415+shivam232323@users.noreply.github.com>
2020-10-16 16:30:37 +0530

    Updates

---
+++ precache-manifest.f68e1e20a5bd21ccdb3e830d4950b93d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39cdc3cd74db2da57dc2191f5fdc5180",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "cf3541c14d35180f0f94",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6967f32b5a00c4452d12",
+    "url": "/robofriends/static/css/main.7dc9747b.chunk.css"
+  },
+  {
+    "revision": "cf3541c14d35180f0f94",
+    "url": "/robofriends/static/js/2.2227c0cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.2227c0cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6967f32b5a00c4452d12",
+    "url": "/robofriends/static/js/main.c39fa4fc.chunk.js"
+  },
+  {
+    "revision": "e38ac1e3243a64411974",
+    "url": "/robofriends/static/js/runtime-main.ead9dd81.js"
+  },
+  {
+    "revision": "65c893d0237c8a87af6e7423f8d3786a",
+    "url": "/robofriends/static/media/Cosmology.65c893d0.ttf"
+  }
+]);
